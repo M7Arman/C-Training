5f729080a73c710fb8fe012fd4bd1cebc1c690bb
Arman Kostandyan <arman_k@instigatemobile.com>
2019-05-03 12:04:26 +0400

    Added comments

--- linked-list-3/list/list.h
+++ linked-list-3/list/list.h
@@ -7,16 +7,34 @@ typedef struct node
   struct node *next;
 } node;
 
+/**
+ * Creates a new node with the given value
+ */
 node* newNode(int value);
 
+/**
+ * Create a list, which nodes count is n
+ */
 node* initialize(int n);
 
+/**
+ * Sets consecutive integer values to the list nodes started with 1
+ */
 node* label(node* list);
 
+/**
+ * Reverses the given list
+ */
 node* reverse(node* list);
 
+/**
+ * Reverses each N nodes of the given list 
+ */
 node* reverseN(int N, node* list);
 
+/**
+ * Prints the given list
+ */
 void print(node* list);
 
 #endif
